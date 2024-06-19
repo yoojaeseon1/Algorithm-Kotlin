d2e1f2c5a11fc2aceea1c9d94435b5419b7538cf
yoojs <yoojs3695@gmail.com>
2024-06-19 23:23:19 +0900

    생성 - CuttingArray

---
+++ src/array/CuttingArray.kt
@@ -0,0 +1,26 @@
+package array
+
+// 87
+fun main() {
+
+    val cuttingArray = CuttingArray()
+
+    println(cuttingArray.solution(4,7,14).contentToString())
+
+}
+class CuttingArray {
+    fun solution(n: Int, left: Long, right: Long): IntArray {
+        val arraySize = right - left + 1
+        val array = IntArray(arraySize.toInt())
+        var arrayIndex = 0
+
+        for(newArrayNumber in left..right) {
+           val newArrayI = newArrayNumber / n
+           val newArrayJ = newArrayNumber % n
+           array[arrayIndex++] = Math.max(newArrayI, newArrayJ).toInt() + 1
+        }
+
+        return array
+    }
+
+}
