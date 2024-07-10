ea8491c0061c3f6294efa9d0b85c74a653a5f605
yoojs <yoojs3695@gmail.com>
2024-07-10 12:58:55 +0900

    생성 MaxNumber / FindSosu

---
+++ src/DFS/FindSosu.kt
@@ -0,0 +1,83 @@
+fun main() {
+
+    val findSosu = FindSosu()
+
+    val numbers = "011"
+
+    println(findSosu.solution(numbers))
+
+
+
+}
+
+class FindSosu {
+
+    val allNumberCases = mutableSetOf<Int>()
+
+    fun solution(numbers: String): Int {
+        var answer = 0
+
+        val numbersToChar = CharArray(numbers.length)
+
+        for ((index, number) in numbers.withIndex()) {
+            numbersToChar[index] = number
+        }
+
+//        val isVisited = BooleanArray(numbers.length)
+//        isVisited[0] = true
+//        val currentNumber = StringBuilder()
+//        currentNumber.append(numbersToChar[0])
+//
+//        selectNumber(numbersToChar, currentNumber, isVisited)
+        selectNumber(numbersToChar, StringBuilder(), BooleanArray(numbers.length))
+
+        for (allNumberCase in allNumberCases) {
+            if(isSosu(allNumberCase)) {
+                println(allNumberCase)
+                answer++
+            }
+        }
+
+
+        return answer
+    }
+
+
+    fun selectNumber(numbers: CharArray, currentNumber: StringBuilder, isVisited: BooleanArray) {
+//        println(currentNumber)
+        if(currentNumber.length > 0) {
+            allNumberCases.add(currentNumber.toString().toInt())
+//            println(currentNumber)
+        }
+
+        for ((numbersIndex, number) in numbers.withIndex()) {
+            if(!isVisited[numbersIndex]) {
+                currentNumber.append(number)
+                isVisited[numbersIndex] = true
+                selectNumber(numbers, currentNumber, isVisited)
+                currentNumber.deleteCharAt(currentNumber.length - 1)
+                isVisited[numbersIndex] = false
+            }
+        }
+    }
+
+
+    fun isSosu(number: Int): Boolean{
+
+        if(number <= 1)
+            return false
+
+        if(number == 2)
+            return true
+
+        val endNumLoop = Math.sqrt(number.toDouble()).toInt()
+
+        for(divider in 2..endNumLoop) {
+            if(number % divider == 0)
+                return false
+        }
+
+        return true
+    }
+
+}
